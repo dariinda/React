38958eebbb83d344ed3035ce4e67a16ac6fb3a38
Abhi <abhisheksinghshekhawat20@gmail.com>
2024-06-11 22:47:33 +0530

    enabled routing and error messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
+import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
 
 const AppLayout = () => {
   return (
@@ -12,5 +15,22 @@ const AppLayout = () => {
     </div>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+    errorElement : <Error/>,
+  },
+  {
+    path: "/about",
+    element: <About/>
+  },
+  {
+    path: "/ContactUs",
+    element: <ContactUs/>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("head"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
